a25f4385b12669b54fb54194661edacfbe5459a2
wuweili <wuweili@yibaomd.com>
2015-05-18 17:14:27 +0800

    new project

---
+++ CLYC/CLYC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLYC
+//
+//  Created by weili.wu on 15/5/18.
+//  Copyright (c) 2015年 weili.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
