855ba32e3ee69069fc39404bfb590ae2e235cbf5
wuweili <wuweili@yibaomd.com>
2015-07-06 19:56:11 +0800

    no message

--- CLYC/CLYC/Controller/DefaultUser/ApplyCarHistoryViewController.m
+++ CLYC/CLYC/Controller/DefaultUser/ApplyCarHistoryViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "ApplyCarHistoryViewController.h"
+#import "MJRefresh.h"
 
-@interface ApplyCarHistoryViewController ()
+
+@interface ApplyCarHistoryViewController ()<UITextFieldDelegate,UITableViewDataSource,UITableViewDelegate>
 {
     UISegmentedControl *_segmentedSwitchView;
     
     NSInteger switchIndex;
+    
+    UIView *_conditionView;
+    
+    UITextField *_projectNumTextField;
+    
+    UITextField *_carNumTextField;
+    
+    
+    UITableView *_tableView;
+    
+    NSMutableArray *_dataArray;
 
 
 }
@@ -34,7 +47,15 @@
     
     self.title = @"约车记录";
     
+    _dataArray = [NSMutableArray arrayWithCapacity:0];
+    
     [self initSwitchView];
+    
+    [self initConditionView];
+    
+    
+    
+    
     
     
 }
@@ -54,6 +75,104 @@
     [self.view addSubview:_segmentedSwitchView];
 }
 
+
+-(void)initConditionView
+{
+    _conditionView = [[UIView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(_segmentedSwitchView.frame)+8, kMainScreenWidth
+                                                             , 110)];
+    
+    _conditionView.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:_conditionView];
+    
+    //项目号
+    
+    UILabel *startTimeLabel = [[UILabel alloc]initWithFrame:CGRectMake(5, 5, 70, 30)];
+    startTimeLabel.font = HEL_14;
+    startTimeLabel.textColor = [UIColor blackColor];
+    startTimeLabel.textAlignment = NSTextAlignmentRight;
+    startTimeLabel.backgroundColor = [UIColor clearColor];
+    startTimeLabel.text = @"项目号：";
+    [_conditionView addSubview:startTimeLabel];
+
+    
+    _projectNumTextField = [[UITextField alloc] initWithFrame:CGRectMake(CGRectGetMaxX(startTimeLabel.frame), startTimeLabel.frame.origin.y , kMainScreenWidth-CGRectGetMaxX(startTimeLabel.frame)-10, 30)];
+    _projectNumTextField.placeholder=@"请输入项目号";
+    _projectNumTextField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    _projectNumTextField.autocapitalizationType=UITextAutocapitalizationTypeNone;
+    _projectNumTextField.font = HEL_15;
+    _projectNumTextField.layer.borderColor=[UIColorFromRGB(0xcccccc) CGColor];
+    _projectNumTextField.layer.cornerRadius = 4.0;
+    _projectNumTextField.layer.borderWidth = 1.0;
+    _projectNumTextField.delegate = self;
+    _projectNumTextField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    [_conditionView addSubview:_projectNumTextField];
+
+    
+    
+    //车辆号
+    
+    
+    UILabel *endTimeLabel = [[UILabel alloc]initWithFrame:CGRectMake(5, CGRectGetMaxY(startTimeLabel.frame)+5, 70, 30)];
+    endTimeLabel.font = HEL_14;
+    endTimeLabel.textColor = [UIColor blackColor];
+    endTimeLabel.textAlignment = NSTextAlignmentRight;
+    endTimeLabel.backgroundColor = [UIColor clearColor];
+    endTimeLabel.text = @"车辆号：";
+    [_conditionView addSubview:endTimeLabel];
+    
+  
+    
+    _carNumTextField = [[UITextField alloc] initWithFrame:CGRectMake(CGRectGetMaxX(endTimeLabel.frame), endTimeLabel.frame.origin.y , kMainScreenWidth-CGRectGetMaxX(endTimeLabel.frame)-10, 30)];
+    _carNumTextField.placeholder=@"请输入车辆号";
+    _carNumTextField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    _carNumTextField.autocapitalizationType=UITextAutocapitalizationTypeNone;
+    _carNumTextField.font = HEL_15;
+    _carNumTextField.layer.borderColor=[UIColorFromRGB(0xcccccc) CGColor];
+    _carNumTextField.layer.cornerRadius = 4.0;
+    _carNumTextField.layer.borderWidth = 1.0;
+    _carNumTextField.delegate = self;
+    _carNumTextField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    [_conditionView addSubview:_carNumTextField];
+    
+    
+    UIButton *startSearchButton = [[UIButton alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(startTimeLabel.frame)+10, kMainScreenWidth-20, 30)];
+    [startSearchButton setBackgroundImage:[UIImage imageNamed:@"button_search.png"] forState:UIControlStateNormal];
+    
+    [startSearchButton setBackgroundImage:[UIImage imageNamed:@"button_search_select.png"] forState:UIControlStateHighlighted];
+    
+    [startSearchButton setTitle:@"查询" forState:UIControlStateNormal];
+    
+    [startSearchButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [startSearchButton addTarget:self action:@selector(clickStartSearchButton) forControlEvents:UIControlEventTouchUpInside];
+    [_conditionView addSubview:startSearchButton];
+    
+}
+
+-(void)initTableView
+{
+    _tableView=[[UITableView alloc]initWithFrame:CGRectMake(0,CGRectGetMaxY(_conditionView.frame),kMainScreenWidth ,kScreenHeightNoStatusAndNoNaviBarHeight) style:UITableViewStylePlain];
+    _tableView.delegate=self;
+    _tableView.dataSource=self;
+    _tableView.showsVerticalScrollIndicator = NO;
+    _tableView.backgroundColor = [UIColor clearColor];
+    _tableView.backgroundView = nil;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:_tableView];
+}
+
+
+-(void)initRefreshView
+{
+    [_tableView addHeaderWithTarget:self action:@selector(headerRefreshing)];
+    
+    [_tableView addFooterWithTarget:self action:@selector(footerRefreshing)];
+    
+    [self tableViewMJRefresh:_tableView];
+}
+
 #pragma  mark - UISegmentedControl  -
 
 -(void)segmentChanges:(UISegmentedControl *)paramSender
@@ -71,6 +190,79 @@
     
     
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    
+    
+    
+    
+    
+    
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_dataArray count];
+}
+
+#pragma mark - 点击查询 - 
+
+-(void)clickStartSearchButton
+{
+    
+}
+
+
+#pragma mark - 下拉刷新 -
+
+-(void)headerRefreshing
+{
+//    _isDownPullLoading = YES;
+//    
+//    _currentDoctorPageIndex = PAGE_INDEX_START;
+//    
+//    [_doctorArray removeAllObjects];
+//    
+//    [self loadAllDoctorDataFromNetWithNoHub:NO];
+    
+}
+
+#pragma mark - 上拉刷新 -
+
+-(void)footerRefreshing
+{
+//    _isUpPullLoading = YES;
+//    
+//    [self loadAllDoctorDataFromNetWithNoHub:NO];
+    
+}
+
+
+
+//如果有更多数据，
+-(void)stopRefresh
+{
+//    if (_isDownPullLoading)
+//    {
+//        _isDownPullLoading = NO;
+//        [_doctorTableView headerEndRefreshing];
+//    }
+//    
+//    if (_isUpPullLoading)
+//    {
+//        _isUpPullLoading = NO;
+//        [_doctorTableView footerEndRefreshing];
+//    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
