d7e4a32db465c07343b8d3d9ff286bc7406b8de9
wuweili <xueye1989@live.cn>
2015-07-09 00:34:39 +0800

    no message

---
+++ CLYC/CLYC/Controller/DefaultUser/ConfirmMileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfirmMileViewController.h
+//  CLYC
+//
+//  Created by wuweiqing on 15/7/8.
+//  Copyright (c) 2015年 weili.wu. All rights reserved.
+//
+
+#import "HXBJHXBaseViewController.h"
+
+@interface ConfirmMileViewController : HXBJHXBaseViewController
+
+@end
