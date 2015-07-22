9ba0d9f82ccec6f4248399bc771af01048d95e87
wuweili <wuweili@yibaomd.com>
2015-07-22 17:50:06 +0800

    no message

--- CLYC/CLYC/View/Home/SaveApplyCarTableViewCell.m
+++ CLYC/CLYC/View/Home/SaveApplyCarTableViewCell.m
@@ -34,33 +34,131 @@ const static int titleLabelwidth = 80;
 
         if (indexPath.row == 0)
         {
+            //用车时间
+            _cellTextView = [[UITextView alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
+            _cellTextView.textColor = [UIColor blackColor];
+            _cellTextView.scrollEnabled = YES;
+            _cellTextView.font = HEL_15;
+            _cellTextView.returnKeyType = UIReturnKeyDone;
+            _cellTextView.userInteractionEnabled = NO;
+            //            _cellTextView.delegate = self;
+            _cellTextView.tag = 1000;
+            _cellTextView.showsVerticalScrollIndicator = NO;
             
+            _cellTextView.backgroundColor = [UIColor clearColor];
+            if (CurrentSystemVersion >= 7.0) {
+                
+                UIEdgeInsets inset = _cellTextView.textContainerInset;
+                
+                _cellTextView.textContainerInset = UIEdgeInsetsMake(inset.top,-4,inset.bottom,-4);
+            }
+            [self.contentView addSubview:_cellTextView];
         }
         else if (indexPath.row == 1)
         {
+            _cellContentLabel = [[UILabel alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
             
+            _cellContentLabel.textColor = [UIColor blackColor];
+            _cellContentLabel.font = HEL_15;
+            _cellContentLabel.textAlignment = NSTextAlignmentLeft;
+            _cellContentLabel.backgroundColor = [UIColor clearColor];
+            [self.contentView addSubview:_cellContentLabel];
         }
         else if (indexPath.row == 2)
         {
+            _cellContentLabel = [[UILabel alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
             
+            _cellContentLabel.textColor = [UIColor blackColor];
+            _cellContentLabel.font = HEL_15;
+            _cellContentLabel.textAlignment = NSTextAlignmentLeft;
+            _cellContentLabel.backgroundColor = [UIColor clearColor];
+            [self.contentView addSubview:_cellContentLabel];
         }
         else if (indexPath.row == 3)
         {
+            _cellContentLabel = [[UILabel alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
             
+            _cellContentLabel.textColor = [UIColor blackColor];
+            _cellContentLabel.font = HEL_15;
+            _cellContentLabel.textAlignment = NSTextAlignmentLeft;
+            _cellContentLabel.backgroundColor = [UIColor clearColor];
+            [self.contentView addSubview:_cellContentLabel];
         }
         else if (indexPath.row == 4)
         {
+            //用车时间
+            _cellTextView = [[UITextView alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
+            _cellTextView.textColor = [UIColor blackColor];
+            _cellTextView.scrollEnabled = YES;
+            _cellTextView.font = HEL_15;
+            _cellTextView.returnKeyType = UIReturnKeyDone;
+            _cellTextView.userInteractionEnabled = NO;
+            //            _cellTextView.delegate = self;
+            _cellTextView.tag = 1004;
+            _cellTextView.showsVerticalScrollIndicator = NO;
             
+            _cellTextView.backgroundColor = [UIColor clearColor];
+            if (CurrentSystemVersion >= 7.0) {
+                
+                UIEdgeInsets inset = _cellTextView.textContainerInset;
+                
+                _cellTextView.textContainerInset = UIEdgeInsetsMake(inset.top,-4,inset.bottom,-4);
+            }
+            [self.contentView addSubview:_cellTextView];
         }
         else if (indexPath.row == 5)
         {
+            //用车时间
+            _cellTextView = [[UITextView alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
+            _cellTextView.textColor = [UIColor blackColor];
+            _cellTextView.scrollEnabled = YES;
+            _cellTextView.font = HEL_15;
+            _cellTextView.returnKeyType = UIReturnKeyDone;
+            _cellTextView.userInteractionEnabled = NO;
+            //            _cellTextView.delegate = self;
+            _cellTextView.tag = 1005;
+            _cellTextView.showsVerticalScrollIndicator = NO;
             
+            _cellTextView.backgroundColor = [UIColor clearColor];
+            if (CurrentSystemVersion >= 7.0) {
+                
+                UIEdgeInsets inset = _cellTextView.textContainerInset;
+                
+                _cellTextView.textContainerInset = UIEdgeInsetsMake(inset.top,-4,inset.bottom,-4);
+            }
+            [self.contentView addSubview:_cellTextView];
         }
         else if (indexPath.row == 6)
         {
+            //用车时间
+            _cellTextView = [[UITextView alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
+            _cellTextView.textColor = [UIColor blackColor];
+            _cellTextView.scrollEnabled = YES;
+            _cellTextView.font = HEL_15;
+            _cellTextView.returnKeyType = UIReturnKeyDone;
+            _cellTextView.userInteractionEnabled = NO;
+            _cellTextView.tag = 1006;
+            _cellTextView.showsVerticalScrollIndicator = NO;
             
+            _cellTextView.backgroundColor = [UIColor clearColor];
+            if (CurrentSystemVersion >= 7.0) {
+                
+                UIEdgeInsets inset = _cellTextView.textContainerInset;
+                
+                _cellTextView.textContainerInset = UIEdgeInsetsMake(inset.top,-4,inset.bottom,-4);
+            }
+            [self.contentView addSubview:_cellTextView];
         }
-        
+        else
+        {
+            _cellContentLabel = [[UILabel alloc]initWithFrame:CGRectMake(_cellTitleLabel.frame.origin.x+_cellTitleLabel.frame.size.width+2, _cellTitleLabel.frame.origin.y, contentMaxWidth, 30)];
+            
+            _cellContentLabel.textColor = [UIColor blackColor];
+            _cellContentLabel.font = HEL_15;
+            _cellContentLabel.textAlignment = NSTextAlignmentLeft;
+            _cellContentLabel.backgroundColor = [UIColor clearColor];
+            [self.contentView addSubview:_cellContentLabel];
+        }
         
         
         
